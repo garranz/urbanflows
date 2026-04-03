4115f2c595f071c574a5479fa64121f8bdeff024
garranz <garranz@TIFA.MIT.EDU>
2026-04-03 16:51:12 -0400

    update input generation

--- scripts_stl/createcase.py
+++ scripts_stl/createcase.py
@@ -10,14 +10,17 @@ if __name__ == "__main__":
 
     # Domain:
     y_offset = 400
-    x_offsets = ( y_offset, 1200 )
+    #x_offsets = ( y_offset, 1200 )
+    x_offsets = ( 1200, 0 )
 
     # Create all the buildings and the box
-    json2stls( output_json, cityname, x_offsets, y_offset, z_offset=300 )
+    json2stls( output_json, cityname, x_offsets, y_offset, z_offset=300, 
+              domain='cylinder' )
 
     # Create the flow volume
     #stl_name = f"{cityname}_x{x_offsets[0]+x_offsets[1]}_y{2*y_offset}_dummy"
-    #fullstl( cityname, stl_name )
+    stl_name = f"{cityname}_r{x_offsets[0]}_dummy"
+    fullstl( cityname, stl_name )
 
 
 
